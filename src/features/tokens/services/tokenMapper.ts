c44e06d909a6c64c721e5f76f2cf868d6d176f04
Mattia <mp.web3@gmail.com>
2026-07-27 15:53:14 +0200

    feat: add token mapper and icon lookup

--- src/features/tokens/services/tokenMapper.ts
+++ src/features/tokens/services/tokenMapper.ts
@@ -1,16 +1,14 @@
-import type { Token } from '@shared/types';
+import { TOKEN_ICONS } from '@shared/assets/tokens'
 import type { TurbineToken } from '@shared/services/turbine/types'
-import { TOKEN_ICONS } from '@shared/assets/tokens';
+import type { Token } from '@shared/types'
 
-
-export function mapToken (supportedTokens: TurbineToken[]): Token[] {
-
-    const tokens: Token[] = supportedTokens.map((token) => ({
-        address: token.address,
-        class: token.class,
-        decimals: token.decimals,
-        icon: TOKEN_ICONS[token.address.toLowerCase()],
-        symbol: token.symbol
-    }))
-    return tokens
+export function mapToken(supportedTokens: TurbineToken[]): Token[] {
+  const tokens: Token[] = supportedTokens.map((token) => ({
+    address: token.address,
+    class: token.class,
+    decimals: token.decimals,
+    icon: TOKEN_ICONS[token.address.toLowerCase()],
+    symbol: token.symbol,
+  }))
+  return tokens
 }
